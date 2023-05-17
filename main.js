c50e5f36c0ee8572ac6aef846b30d9198c520384
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-05-17 14:14:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 let isLive = false;
-let points = [{x: 0, y: window.innerHeight + 60}];
-let dy;
+let points1 = [{x: 0, y: window.innerHeight + 60}]; //Person der kigger på
+let points2 = [{x: 0, y: window.innerHeight + 60}]; //Astronaut Selv
+let dy1;
+let dy2;
 let h;
 const c = 299792;
 let t = 0; //Svarer til X
 let r = 1000; //Svarer til Y
+let r2 = 1000
 const rs = 25;
 let y;
+let y2;
 let x;
 
 function setup() {
   console.log("Made By Lucas L, Source: https://github.com/LucasFromDK/SRC-Opgave");
-  dy = -c * (1 - rs / r) * sqrt(rs / r);
+  dy1 = -c * (1 - rs / r) * sqrt(rs / r);
+  dy2 = -c * sqrt(rs / r);
   startButton = createButton("Start").mousePressed(startAnimation);
   startButton.position(windowWidth-400, windowHeight - startButton.height);
   skridtSlider = createSlider(0.0001, 0.0005, 0.0001, 0.0001);
   skridtSlider.position(startButton.x + 130, startButton.y);
   createCanvas(windowWidth, windowHeight);
   frameRate(30);
-  return dy;
+  return dy1, dy2;
 }
 
 function draw() {
   background(220);
   UI()
   drawPoints(); //Tegner Punkterne
+  astronautView();
   if (isLive == true) {
     eulerMethod();
   }
@@ -32,8 +38,12 @@ function draw() {
 
 function UI() {
   noStroke()
+  fill("blue")
+  text("Observatørens Synspunkt", 5, windowHeight-80)
+  fill("red")
+  text("Astronautens Synspunkt", 5, windowHeight-65)
   fill("black")
-  text("Hastigheds ændring: " + Math.round(dy) + " km/s  |  " + "Tid gået: " + t + " s  |  " + "Distance Tilbage: " + Math.round(r) + " km  |  " + "Antal Punkter: " + points.length, 5, windowHeight - 5);
+  text("Hastigheds ændring: " + Math.round(dy1) + " km/s" + "\nTid gået: " + t + " s" + "\nDistance Tilbage: " + Math.round(r) + " km" + "\nAntal Punkter: " + points1.length, 5, windowHeight - 50);
   text("Skridtlængde: ", skridtSlider.x - 75, startButton.y + 14);
   text("Skridtlængde er: " + skridtSlider.value(), skridtSlider.x + 140, startButton.y + 14);
 }
@@ -54,38 +64,63 @@ function startAnimation() {
 
 function eulerMethod() {
   y = r;
+  y2 = r2;
   x = t;
   h = skridtSlider.value();
 
-  if (r >= 25) {
+  if (r >= Math.ceil(rs)) {
     t = t + h;
-    const ychange = -c * (1 - rs / y) * sqrt(rs / y);
+    const ychange = -c * (1 - rs / y) * sqrt(rs / y); //Person der kigger på formel
+    const astro = -c * sqrt(rs / y2); //Astronaut formel
     r += ychange * h;
+    r2 += astro * h;
 
+    console.log(astro)
     //Send punkter til opbevaring i arrayet points
-    points.push({ x: t * window.innerWidth*45, y: y });
-    dy = ychange;
-    console.log("Distance: " + Math.round(r) + " km", "\nSpeed: " + Math.round(ychange) + " km/s", "\nTid gået: " + t + " s", "\nAntal Punkter: " + points.length);
-    return t, h, y, dy;
+    points1.push({ x: t * window.innerWidth*45, y: y });
+    points2.push({ x: t * window.innerWidth*45, y: y2 });
+    dy1 = ychange;
+    console.log("Distance: " + r + " km", "\nSpeed: " + Math.round(ychange) + " km/s", "\nTid gået: " + t + " s", "\nAntal Punkter: " + points1.length);
+    return t, h, y, dy1;
   }
 }
 
 function drawPoints() {
-  // Tegner Punkterne
-  for (let i = 0; i < points.length; i++) {
+  // Tegner Punkterne for personen der kigger på.
+  for (let i = 0; i < points1.length; i++) {
     strokeWeight(2);
-    let punkt = points[i];
+    let punkt = points1[i];
     point(punkt.x, map(punkt.y, 0, 1000, height+60, 0));
   }
 
-  // Forbinder punkter med en linje
+  //Forbinder punkter med en linje for person der kigger på.
   strokeWeight(1);
   stroke("blue")
   noFill();
   beginShape();
-  for (let i = 0; i < points.length; i++) {
-    let punkt = points[i];
+  for (let i = 0; i < points1.length; i++) {
+    let punkt = points1[i];
     vertex(punkt.x, map(punkt.y, 0, 1000, height+60, 0));
   }
   endShape();
+}
+
+function astronautView() {
+    // Tegner punkterne for astronauten selv.
+    for (let i = 0; i < points2.length; i++) {
+      strokeWeight(2);
+      let punkt = points2[i];
+      point(punkt.x, map(punkt.y, 0, 1000, height+60, 0));
+    }
+  
+    //Forbinder punkter med en linje for astronauten selv.
+    strokeWeight(1);
+    stroke("red")
+    noFill();
+    beginShape();
+    for (let i = 0; i < points2.length; i++) {
+      let punkt = points2[i];
+      vertex(punkt.x, map(punkt.y, 0, 1000, height+60, 0));
+    }
+    endShape();
 }
