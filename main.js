002d7e6b62ad056e70b12274b107b883a86c1b9f
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-05-19 09:17:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ function setup() {
   dy2 = -c * sqrt(rs / r);
   startButton = createButton("Start").mousePressed(startAnimation);
   startButton.position(windowWidth-400, windowHeight - startButton.height);
+  resetButton = createButton("Reset").mousePressed(reset)
+  resetButton.position(startButton.x-resetButton.width-5, windowHeight - startButton.height);
   skridtSlider = createSlider(0.0001, 0.0005, 0.0001, 0.0001);
   skridtSlider.position(startButton.x + 130, startButton.y);
   createCanvas(windowWidth, windowHeight);
@@ -43,7 +45,7 @@ function UI() {
   fill("red")
   text("Astronautens Synspunkt", 5, windowHeight-65)
   fill("black")
-  text("Hastigheds ændring: " + Math.round(dy1) + " km/s" + "\nTid gået: " + t + " s" + "\nDistance Tilbage: " + Math.round(r) + " km" + "\nAntal Punkter: " + points1.length, 5, windowHeight - 50);
+  text("Hastigheds ændring: " + Math.round(dy1) + " km/s" + "\nTid gået (x-akse): " + t + " s" + "\nDistance tilbage til centrum (y-akse): " + Math.round(r) + " km" + "\nAntal Punkter: " + points1.length, 5, windowHeight - 50);
   text("Skridtlængde: ", skridtSlider.x - 75, startButton.y + 14);
   text("Skridtlængde er: " + skridtSlider.value(), skridtSlider.x + 140, startButton.y + 14);
 }
@@ -68,14 +70,14 @@ function eulerMethod() {
   x = t;
   h = skridtSlider.value();
 
-  if (r >= Math.ceil(rs)) {
+  //Begrænset til 153 for at fikse en fejl med at grafen kan fortsætte til en værdi lavere ens rs.
+  if (r >= rs && points1.length < 153) {
     t = t + h;
     const ychange = -c * (1 - rs / y) * sqrt(rs / y); //Person der kigger på formel
     const astro = -c * sqrt(rs / y2); //Astronaut formel
     r += ychange * h;
     r2 += astro * h;
 
-    console.log(astro)
     //Send punkter til opbevaring i arrayet points
     points1.push({ x: t * window.innerWidth*45, y: y });
     points2.push({ x: t * window.innerWidth*45, y: y2 });
@@ -106,6 +108,7 @@ function drawPoints() {
 }
 
 function astronautView() {
+  noStroke()
     // Tegner punkterne for astronauten selv.
     for (let i = 0; i < points2.length; i++) {
       strokeWeight(2);
@@ -123,4 +126,22 @@ function astronautView() {
       vertex(punkt.x, map(punkt.y, 0, 1000, height+60, 0));
     }
     endShape();
+}
+
+function reset() {
+console.log("Reset Animation + Values")
+ isLive = false;
+ startButton.html("Start");
+ points1 = [{x: 0, y: window.innerHeight + 60}]; //Person der kigger på
+ points2 = [{x: 0, y: window.innerHeight + 60}]; //Astronaut Selv
+ dy1 = -46216
+ dy2;
+ h;
+ t = 0; //Svarer til X
+ r = 1000; //Svarer til Y
+ r2 = 1000
+ y;
+ y2;
+ x;
+ return isLive, points1, points2, dy1, dy2, h, t, r, r2, y, y2, x
 }
